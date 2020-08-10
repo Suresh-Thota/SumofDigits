20a17e52424f4c186088b8522be54b6668ad6405
Suresh-Thota <Suresh.Thota@innroad.com>
2020-08-10 21:33:20 +0530

    Add project files.

---
+++ SumofDigits/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SumofDigits
+{
+    class Program
+    {
+        public static int Sum(int n)
+        {
+            if (n / 10 == 0)
+                return n;
+            return Sum(n / 10) + n % 10;
+        }
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("enter a number");
+
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Sum of digits of number is " + Sum(n));
+        }
+    }
+}
